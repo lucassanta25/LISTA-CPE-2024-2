844450a177b2782a1be259daf71eee2b1932638a
lucassanta25 <145135612+lucassanta25@users.noreply.github.com>
2024-12-19 11:52:24 -0300

    Update Exercício 5.cpp

--- LISTA3/Exercício 5.cpp
+++ LISTA3/Exercício 5.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    char caractere;
+
+    cout << "Digite um caractere: ";
+    cin >> caractere;
+
+    switch (caractere) {
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+            cout << "O caractere " << caractere << " é uma vogal." << endl;
+            break;
+        default:
+            cout << "O caractere " << caractere << " não é uma vogal." << endl;
+    }
+
+    return 0;
+}
