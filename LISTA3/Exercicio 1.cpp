0dc25ab597aeddfc1cf5c0577879e8ab6cf7fa1a
lucassanta25 <145135612+lucassanta25@users.noreply.github.com>
2024-12-19 08:57:24 -0300

    Update Exercicio 1.cpp

--- LISTA3/Exercicio 1.cpp
+++ LISTA3/Exercicio 1.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 using namespace std;
 
-
 int main()
 {
-   double QL;
-   double QKP;
-   double total_gasto;
-    
-    cout << "insira a quantidade de litros inseridos:" << endl;
+    double QL, QKP, total_gasto;
+
+    cout << "Insira a quantidade de litros inseridos: ";
     cin >> QL;
-    cout << "insira a quantidade de kilometros pecorridos:" << endl;
+    cout << "Insira a quantidade de quilometros percorridos: ";
     cin >> QKP;
-    if(QL > 0 && QKP > 0){
-        
-      total_gasto = QKP/QL;
-      cout << "o consumo medio do carro é: " << total_gasto << endl;
-      
-    }else{
-        cout << "valor invalido, digite um valor superior a 0:"<<endl;
+
+    if (QL > 0 && QKP > 0) {
+        total_gasto = QKP / QL;
+        cout << "O consumo medio do carro é: " << total_gasto << " km/l" << endl;
+    } else {
+        cout << "Valor invalido, digite valores superiores a 0." << endl;
     }
+
     return 0;
 }
