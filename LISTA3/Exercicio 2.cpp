a85dc0d261369fe4da1075f3e417d5ee7e088394
lucassanta25 <145135612+lucassanta25@users.noreply.github.com>
2024-12-19 09:45:14 -0300

    Update Exercicio 2.cpp

--- LISTA3/Exercicio 2.cpp
+++ LISTA3/Exercicio 2.cpp
@@ -8,20 +8,21 @@ int main()
    double gasolina;
    double calculo;
    
-   cout << "insira o valor do litro da gasolina" << endl;
+   cout << "insira o valor do litro da gasolina:" << endl;
    cin >> gasolina;
-   cout << "insira o valor do litro do alcool" << endl;
+   cout << "insira o valor do litro do alcool:" << endl;
    cin >> alcool;
    
    calculo = alcool / gasolina;
    
-  if (calculo < 0,7){
+  if (calculo < 0.7){
       cout << "é melhor abastecer com álcool" << endl;
   }else{
       cout << "é melhor abastecer com gasolina" << endl;
   }
    
-   
+   cout << "valor litro alcool:" << alcool<< "      " << "valor litro gasolina:" << gasolina << endl;
+   cout << "valor da diferença" << calculo << endl;
    
     return 0;
 }
