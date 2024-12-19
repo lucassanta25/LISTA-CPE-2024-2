2601c0721296239a10711d0058f82d95b7148903
lucassanta25 <145135612+lucassanta25@users.noreply.github.com>
2024-12-19 11:42:48 -0300

    Update Exercício 4.cpp

--- LISTA3/Exercício 4.cpp
+++ LISTA3/Exercício 4.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int binario, digito1, digito2, digito3, digito4, decimal = 0;
+    
+    cout << "Digite um numero binario de 4 digitos: ";
+    cin >> binario;
+
+    digito4 = binario % 10;          
+    binario /= 10;
+    digito3 = binario % 10;          
+    binario /= 10;
+    digito2 = binario % 10;          
+    binario /= 10;
+    digito1 = binario % 10;          
+
+    if (digito1 == 1) {
+        decimal += 8;  
+    }
+    if (digito2 == 1) {
+        decimal += 4;  
+    }
+    if (digito3 == 1) {
+        decimal += 2;  
+    }
+    if (digito4 == 1) {
+        decimal += 1;  
+    }
+
+    cout << "O valor decimal é: " << decimal << endl;
+
+    return 0;
+}
