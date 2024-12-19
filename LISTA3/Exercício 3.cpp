a119d01d0070198e3077c80a0ff441afee36d7bc
lucassanta25 <145135612+lucassanta25@users.noreply.github.com>
2024-12-19 10:05:19 -0300

    Update Exercício 3.cpp

--- LISTA3/Exercício 3.cpp
+++ LISTA3/Exercício 3.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int digito1, digito2, digito3, digito4, digito5;
+    
+    cout << "insira os 5 digitos para verificar se é ou não um palimedro" << endl;
+    cout << "digito 1:" << endl;
+    cin >> digito1;
+    cout << "digito 2:" << endl;
+    cin >> digito2;
+    cout << "digito 3:" << endl;
+    cin >> digito3;
+    cout << "digito 4:" << endl;
+    cin >> digito4;
+    cout << "digito 5:" << endl;
+    cin >> digito5;
+    
+    if(digito1 == digito5 && digito2 ==digito4){
+        cout <<"é um palimedro" << endl;
+        
+    }else{
+        cout << "não é um palimedro" << endl;
+    }
+    return 0;
+}
