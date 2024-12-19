9271ef8e626db9d966cecced2feac87b7ee048a2
lucassanta25 <145135612+lucassanta25@users.noreply.github.com>
2024-12-19 12:13:46 -0300

    Update Exercício 6.cpp

--- LISTA3/Exercício 6.cpp
+++ LISTA3/Exercício 6.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+ int LadoA, LadoB, LadoC;
+ 
+ cout << "insira o lado A do triangulo: " << endl;
+ cin >> LadoA;
+ cout << "insira o lado B do triangulo: " << endl;
+ cin >> LadoB;
+ cout << "insira o lado C do triangulo: " << endl;
+ cin >> LadoC;
+ 
+ if(LadoA ==LadoB && LadoA == LadoC && LadoB==LadoC){
+     cout << "Triangulo equilátero" << endl;
+ }else if (LadoA==LadoB || LadoB==LadoC || LadoA==LadoC){
+     cout << "Triangulo Isocele" << endl;
+ }else {
+     cout << "Triangulo Escaleno" << endl;
+ }
+ 
+    return 0;
+}
